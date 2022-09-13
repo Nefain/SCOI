51415c432d0ce90fbf5aee514c99816d2690f89c
Nefain <breadmaker@list.ru>
2022-09-13 17:43:07 +0300

    Update

---
+++ LabaSCOI2/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SCOI5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(31, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 310);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(500, 31);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(413, 310);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(31, 381);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(413, 310);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(500, 381);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(413, 310);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(951, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1097, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Выполнить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(950, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введите фильтр (x;y;z)";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(951, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(229, 121);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(953, 263);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(174, 21);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Зеркальный Фильтер";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(951, 221);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(228, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(950, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Тип Фильтра";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 721);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LabaSCOI2/Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCOI5
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap image1 = null;
+        TypeFilterFreq Type;
+        public Form1()
+        {
+            string[] Filter = new string[2];
+            Filter[0] = "Высокочастотный";
+            Filter[1] = "Низкочастотный";
+            InitializeComponent();
+            comboBox1.Items.Add(Filter[0]);
+            comboBox1.Items.Add(Filter[1]);
+            comboBox1.SelectedItem = Filter[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog.Filter = "Картинки (png, jpg, bmp, gif) |*.png;*.jpg;*.bmp;*.gif|All files (*.*)|*.*";
+
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (image1 != null)
+                {
+                    pictureBox1.Image = null;
+                    
+                }
+
+                image1 = new Bitmap(openFileDialog.FileName);
+                var image2 = new Bitmap(image1, new Size(pictureBox1.Size.Width, pictureBox1.Size.Height));
+                pictureBox1.Image = image2;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool flag = false;
+            Type = (TypeFilterFreq)comboBox1.SelectedIndex;
+            var Filters = richTextBox1.Text.Split('\n');
+            List<FilterCircle> Spisok = new List<FilterCircle>();
+            foreach (var filter in Filters)
+            {
+                Spisok.Add(new FilterCircle(filter));
+            }
+            if(checkBox1.Checked == true)
+            {
+                flag = true;
+            }
+            var Images = Img.Obrabotka(image1, Spisok, Type, flag);
+            pictureBox2.Image = new Bitmap(Images.NewImage, new Size(pictureBox2.Size.Width, pictureBox2.Size.Height));
+            pictureBox3.Image = new Bitmap(Images.FilterImage, new Size(pictureBox2.Size.Width, pictureBox2.Size.Height));
+            pictureBox4.Image = new Bitmap(Images.FourierImage, new Size(pictureBox2.Size.Width, pictureBox2.Size.Height));
+        }
+    }
+    public static class FT
+    {
+        public static Complex[] ditfft2(Complex[] arr, int x0, int N, int s)
+        {
+            Complex[] X = new Complex[N];
+            if (N == 1)
+            {
+                X[0] = arr[x0];
+            }
+            else
+            {
+                ditfft2(arr, x0, N / 2, 2 * s).CopyTo(X, 0);
+                ditfft2(arr, x0 + s, N / 2, 2 * s).CopyTo(X, N / 2);
+
+                for (int k = 0; k < N / 2; k++)
+                {
+                    var t = X[k];
+                    double u = -2.0 * Math.PI * k / N;
+                    X[k] = t + new Complex(Math.Cos(u), Math.Sin(u)) * X[k + N / 2];
+                    X[k + N / 2] = t - new Complex(Math.Cos(u), Math.Sin(u)) * X[k + N / 2];
+                }
+            }
+
+            return X;
+        }
+
+        public static Complex[] ditfft2d(Complex[] arr, int width, int height, bool use_FFT = true) //прямое фурье преобразование
+        {
+            Complex[] X = new Complex[arr.Length];
+
+            ParallelOptions opt = new ParallelOptions();
+            if (Environment.ProcessorCount > 2)
+                opt.MaxDegreeOfParallelism = Environment.ProcessorCount - 1;
+            else opt.MaxDegreeOfParallelism = 1;
+            //for (int i = 0; i < height; ++i)
+            Parallel.For(0, height, opt, i =>
+            {
+                Complex[] tmp = new Complex[width];
+                Array.Copy(arr, i * width, tmp, 0, width);
+                tmp = ditfft2(tmp, 0, tmp.Length, 1);
+
+                for (int k = 0; k < width; ++k)
+                    X[i * width + k] = tmp[k] / width;
+            }
+            );
+            //for (int j = 0; j < width; ++j)
+            Parallel.For(0, width, opt, j =>
+            {
+                Complex[] tmp = new Complex[height];
+                for (int k = 0; k < height; ++k)
+                    tmp[k] = X[j + k * width];
+                tmp = ditfft2(tmp, 0, tmp.Length, 1);
+
+                for (int k = 0; k < height; ++k)
+                    X[j + k * width] = tmp[k] / height;
+            }
+            );
+            return X;
+        }
+
+        public static Complex[] ditifft2d(Complex[] arr, int width, int height, bool use_FFT = true) //обратное фурье преобразование
+        {
+            Complex[] X = new Complex[arr.Length];
+
+            ParallelOptions opt = new ParallelOptions();
+            if (Environment.ProcessorCount > 2)
+                opt.MaxDegreeOfParallelism = Environment.ProcessorCount - 1;
+            else opt.MaxDegreeOfParallelism = 1;
+
+            Parallel.For(0, height, opt, i =>
+            {
+                Complex[] tmp = new Complex[width];
+                Array.Copy(arr, i * width, tmp, 0, width);
+                for (int k = 0; k < width; ++k)
+                    tmp[k] = new Complex(arr[i * width + k].Real, -arr[i * width + k].Imaginary);
+
+                tmp = ditfft2(tmp, 0, tmp.Length, 1);
+
+                for (int k = 0; k < width; ++k)
+                    X[i * width + k] = new Complex(tmp[k].Real, -tmp[k].Imaginary);
+
+            }
+            );
+
+            Parallel.For(0, width, opt, j =>
+            {
+                Complex[] tmp = new Complex[height];
+                for (int k = 0; k < height; ++k)
+                    tmp[k] = new Complex(X[j + k * width].Real, -X[j + k * width].Imaginary);
+
+                tmp = ditfft2(tmp, 0, tmp.Length, 1);
+
+                for (int k = 0; k < height; ++k)
+                    X[j + k * width] = (new Complex(tmp[k].Real, -tmp[k].Imaginary));
+            }
+            );
+            return X;
+        }
+    }
+    public static class Img
+    {
+
+        public static Bitmap ToBitmapSource(this byte[] Bytes, int stride, int width, int height)
+        {
+            Bitmap Bimage = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            Rectangle rect = new Rectangle(0, 0, Bimage.Width, Bimage.Height);
+            BitmapData bmpData =
+                Bimage.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+            IntPtr ptr = bmpData.Scan0;
+
+            int bytes = Math.Abs(bmpData.Stride) * Bimage.Height;
+            System.Runtime.InteropServices.Marshal.Copy(Bytes, 0, ptr, bytes);
+
+            Bimage.UnlockBits(bmpData);
+
+            return Bimage;
+        }   
+        public static double F(double x)
+        {
+            return Math.Log(x + 1);
+        }
+        static byte clmp(double d)
+        {
+            if (d > 255)
+                return 255;
+            if (d < 0)
+                return 0;
+            return (byte)d;
+        }
+        public static ResultImages Obrabotka(Bitmap source, List<FilterCircle> Filters, TypeFilterFreq FilterFreq, bool flag)
+        {
+
+            if (source == null)
+            {
+                return new ResultImages() { FilterImage = null, FourierImage = null, NewImage = null };
+            }
+
+            int _stride;
+            int _height = source.Height;
+            int _width = source.Width;
+            int _bitsPerPixel;
+
+            int new_width = _width;
+            int new_height = _height;
+
+            var p = Math.Log(_width,2);
+            if (p != Math.Floor(p))
+                new_width = (int)Math.Pow(2, Math.Ceiling(p));
+            p = Math.Log(_height,2);
+            if (p != Math.Floor(p))
+                new_height = (int)Math.Pow(2, Math.Ceiling(p));
+
+            Bitmap _tmp = new Bitmap(new_width, new_height, PixelFormat.Format32bppArgb);
+            _tmp.SetResolution(source.HorizontalResolution, source.VerticalResolution);
+
+            Graphics g = Graphics.FromImage(_tmp);
+            g.DrawImageUnscaled(source, 0, 0);
+
+            byte[] oldBytes = _tmp.ToByte(out _stride, out _height, out _width);
+            int ink = 4;
+
+            byte[] newBytes = new byte[oldBytes.Length]; //обычное изображение
+            byte[] fourBytes = new byte[oldBytes.Length]; //фурье образ
+            byte[] fourFilterBytes = new byte[oldBytes.Length]; //изображение с фильтрами
+
+            Complex[] complex_bytes = new Complex[_width * _height];
+            int BytesOnPixel = 4;
+            for (int color = 0; color <= 2; color++)
+            {
+
+                for (int i = 0; i < _width * _height; ++i)
+                {
+                    int y = i / _width;
+                    int x = i - y * _width;
+                    complex_bytes[i] = Math.Pow(-1, x + y) * oldBytes[i * BytesOnPixel + color];
+                }
+
+                complex_bytes = FT.ditfft2d(complex_bytes, _width, _height);
+
+                var max_ma = complex_bytes.Max(x => F(x.Imaginary));
+
+                Complex[] complex_bytes_filtered = (Complex[])complex_bytes.Clone();
+                if (Filters != null)
+                {
+                    if (flag == true)
+                    {
+                        Filters.AddMirrors();
+                    }
+
+                    int centerX = _width / 2;
+                    int centerY = _height / 2;
+
+                    for (int i = 0; i < (_width * _height); i++)
+                    {
+
+                        bool inFigure = false;
+                        int y = i / _width;
+                        int x = i % _width;
+                        for (int filterCount = 0; filterCount < Filters.Count; filterCount++)
+                        {
+                            if (Filters[filterCount].FilterCoef(x - centerX, centerY - y) == 1)
+                            {
+                                inFigure = true;
+                                break;
+                            }
+                        }
+                        switch (FilterFreq)
+                        {
+                            case TypeFilterFreq.HightFreq:
+                                {
+                                    if (inFigure)
+                                    {
+                                        complex_bytes_filtered[i] *= 0; //если пиксель не попадает в кружок
+                                    }
+                                    else
+                                    {
+                                        fourFilterBytes[i * ink] = 255;
+                                        fourFilterBytes[i * ink + 1] = 255;
+                                        fourFilterBytes[i * ink + 2] = 255;
+                                    }
+                                    break;
+                                }
+                            case TypeFilterFreq.LowFreq:
+                                {
+                                    if (!inFigure)
+                                    {
+                                        complex_bytes_filtered[i] *= 0;
+                                    }
+                                    else
+                                    {
+                                        fourFilterBytes[i * ink] = 255;
+                                        fourFilterBytes[i * ink + 1] = 255;
+                                        fourFilterBytes[i * ink + 2] = 255;
+                                    }
+                                    break;
+                                }
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+
+                    }
+                }
+                var complex_bytes_result = FT.ditifft2d(complex_bytes_filtered, _width, _height);
+
+                for (int i = 0; i < _width * _height; ++i)
+                {
+                    int y = i / _width;
+                    int x = i - y * _width;
+                    y -= _height / 2;
+                    x -= _width / 2;
+                    newBytes[i * BytesOnPixel + color] = clmp(Math.Round((Math.Pow(-1, x + y) * complex_bytes_result[i]).Real));
+                    fourBytes[i * BytesOnPixel + color] = clmp(10 * F(complex_bytes[i].Magnitude) * 255 / max_ma);
+                }
+            }
+            for(int i = 0; i < _width*_height;i++)
+            {
+                newBytes[i * 4 +3] = fourBytes[i * 4 + 3] = fourFilterBytes[i * 4 + 3] = 255;
+            }
+            return new ResultImages() { NewImage = newBytes.ToBitmapSource(_stride, _width, _height), FourierImage = fourBytes.ToBitmapSource(_stride, _width, _height), FilterImage = fourFilterBytes.ToBitmapSource(_stride, _width, _height) };
+
+        }
+
+        public static byte[] ToByte(this Bitmap bitmaps, out int Stride, out int Height, out int Width) //битмап сурс превращает в массив байтов
+        {
+            Width = bitmaps.Width;
+            Height = bitmaps.Height;
+
+            Rectangle rect = new Rectangle(0, 0, Width, Height);
+            BitmapData bmpData =
+                bitmaps.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+            IntPtr ptr = bmpData.Scan0;
+            Stride = bmpData.Stride;
+            int bytes = Math.Abs(bmpData.Stride) * bitmaps.Height;
+            byte[] bgrAValues = new byte[bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(ptr, bgrAValues, 0, bytes);
+
+            bitmaps.UnlockBits(bmpData);
+
+            return bgrAValues;
+        }
+    }
+    public class FilterCircle
+    {
+        public double Ycenter { get; set; }
+        public double Xcenter { get; set; }
+        public double Radius { get; set; }
+
+        /// <summary>
+        /// 1 если точка внутри круга
+        /// 0 если точка вне круга
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public FilterCircle()
+        {
+
+        }
+        public FilterCircle(string Data)
+        {
+            var nums = Data.Split(';');
+
+            Xcenter = double.Parse(nums[0]);
+            Ycenter = double.Parse(nums[1]);
+            Radius = double.Parse(nums[2]);
+
+        }
+        public double FilterCoef(double x, double y)
+        {
+            if (Math.Pow(x - Xcenter, 2) + Math.Pow(y - Ycenter, 2) < Radius * Radius)
+                return 1;
+            else
+                return 0;
+        }
+
+        public FilterCircle MirrorFilterFigure(FilterCircle filter)
+        {
+            return new FilterCircle() { Xcenter = -((FilterCircle)filter).Xcenter, Ycenter = -((FilterCircle)filter).Ycenter, Radius = ((FilterCircle)filter).Radius };
+        }
+    }
+
+    public static class FilterFigureMirror
+    {
+        public static void AddMirrors(this List<FilterCircle> filters)
+        {
+            int Leng = filters.Count;
+            for (int i = 0; i < Leng; i++)
+            {
+                filters.Add(filters[i].MirrorFilterFigure(filters[i]));
+            }
+        }
+    }
+
+    public enum TypeFilterFreq
+    {
+        HightFreq,
+        LowFreq
+    }
+    public class ResultImages
+    {
+        public ResultImages()
+        {
+        }
+
+        public Bitmap FilterImage { get; set; }
+        public Bitmap FourierImage { get; set; }
+        public Bitmap NewImage { get; set; }
+    }
+}
